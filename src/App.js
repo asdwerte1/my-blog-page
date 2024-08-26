4251d592d9754944a73a0661e983a8a84b48c665
asdwerte1 <samjmarkham@outlook.com>
2024-08-26 14:15:20 +0100

    Setup - removal of default content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Placeholder page - blog in development</h1>
-      </header>
     </div>
   );
 }
-
-export default App;
