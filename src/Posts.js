75aeef1950b786551cf6b6090681309d55476823
asdwerte1 <samjmarkham@outlook.com>
2024-09-01 17:36:54 +0100

    Created class and constructor for post objects

---
+++ src/Posts.js
@@ -0,0 +1,9 @@
+class Post {
+    constructor(title, text, comments, dateTime, topic,) {
+        this.title = title;
+        this.text = text;
+        this.comments = [];
+        this.dateTime = dateTime;
+        this.topic = topic;
+    }
+}
