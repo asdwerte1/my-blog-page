c5655cfe03f1327a1959641ce1a16e054ce93731
asdwerte1 <samjmarkham@outlook.com>
2024-10-21 16:10:27 +0100

    Minor chnages to layout

--- src/pages/Development.jsx
+++ src/pages/Development.jsx
@@ -1,15 +1,17 @@
 const Development = () => {
     return (
-        <header>
-            <h1>Development</h1>
-            <h2>Updates on my home lab development including:</h2>
-            <ul>
-                <li>Web development</li>
-                <li>Programming projects</li>
-                <li>Home lab upgrades</li>
-                <li>And more...</li>
-            </ul>
-        </header>
+        <>
+            <header>
+                <h1>Development</h1>
+                <h2>Updates on my home lab development including:</h2>
+                <ul>
+                    <li>Web development</li>
+                    <li>Programming projects</li>
+                    <li>Home lab upgrades</li>
+                    <li>And more...</li>
+                </ul>
+            </header>
+        </>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 const Home = () => {
     return (
-        <header>
-            <h1>Home</h1>
-            <h2>Welcome to my Blog site!</h2>
-            <p>The home of my (maybe) interesting hobbies and learning, and a bit of fun and practice along the way!</p>
-        </header>
+        <>
+            <header>
+                <h1>Home</h1>
+                <h2>Welcome to my Blog site!</h2>
+                <p>The home of my (maybe) interesting hobbies and learning, and a bit of fun and practice along the way!</p>
+            </header>
+        </>
     )
 }
 
